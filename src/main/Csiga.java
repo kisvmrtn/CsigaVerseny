eca5edc8652445928066f93af4a202f95d1ad007
KisváradiMárton(Szf_ <kisvaradi.marci@gmail.com>
2025-03-27 14:56:18 +0100

    csiga.java feltoltve adatok, tagf

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -1,7 +1,56 @@
 
 package main;
 
+import java.util.Random;
+
 
 public class Csiga {
     
+    static final Random rnd = new Random();
+    
+    private String szin;
+    private String pozicio;
+    private int sebesseg;
+    private boolean gyorsito;
+    
+    public Csiga(String szin) {
+        this.szin = szin;
+    }
+    
+    public String getSzin() {
+        return szin;
+    }
+    
+    public String getPozicio() {
+        return pozicio;
+    }
+    public void setPozicio(String pozicio) {
+        this.pozicio += pozicio;
+    }
+    public int getSebesseg() {
+        return sebesseg;
+    }
+    public void setSebesseg(int sebesseg) {
+        this.sebesseg = sebesseg;
+    }
+    public boolean getGyorsito() {
+        return gyorsito;
+    }
+    
+    public void mozog() {
+        int egysSzam;
+        String egyseg = "";
+        if (this.gyorsito) {
+            egysSzam = rnd.nextInt(0, 7);
+            for (int i = 0; i < egysSzam; i++) {
+            egyseg += "=";
+            }
+        } else {
+            egysSzam = rnd.nextInt(0, 4);
+            for (int i = 0; i < egysSzam; i++) {
+                egyseg += "-";
+            }
+        }
+        System.out.println(egyseg);
+    }
 }
